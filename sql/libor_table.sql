a6b525e0a24c288fee4458c54009b936109b704c
Noe Navarro <eon@Noes-MacBook-Pro.local>
2022-11-25 16:06:56 -0700

    saving main

---
+++ sql/libor_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE libor_rates (
+  `date`            DATE,
+  1_mo_rate         DECIMAL,
+  2_mo_rate         DECIMAL,
+  3_mo_rate         DECIMAL,
+  4_mo_rate         DECIMAL,
+  5_mo_rate         DECIMAL,
+  6_mo_rate         DECIMAL,
+  1_yr_rate         DECIMAL
+);
---
+++ sql/treasury_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE treasury_rates (
+  `date`            DATE,
+  2_yr_rate         DECIMAL,
+  3_yr_rate         DECIMAL,
+  5_yr_rate         DECIMAL,
+  7_yr_rate         DECIMAL,
+  10_yr_rate        DECIMAL,
+  20_yr_rate        DECIMAL,
+  30_yr_rate        DECIMAL
+);
