98f23553c0b0c42c5b75c830a9d6f518eb32cb35
Noe Navarro <eon@Noes-MacBook-Pro.local>
2022-11-27 18:59:21 -0700

    initial upload for treasury rates successful

--- treasury_rate.py
+++ treasury_rate.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import sqlalchemy
+import sql_connection
 
 # establish excel filepath
 filepath = r'./excel/MRKTRATE - 09.21.2022.xlsx'
@@ -17,10 +17,8 @@ for col in col_names[1:]:
     df[col] = df[col].astype('float') / 100
 
 # upload to sql
-conn = sql_connection.conn('market_data')
-cursor = conn.cursor()
-cursor.close()
-conn.close()
+engine = sql_connection.conn('market_data', alchemy=True)
+df.to_sql('treasury_rates', con=engine, if_exists='replace', index=False)
 
-# upload to sql
-df.to_sql('treasury_rates', con=conn, if_exists='replace', index=False)
+# close connection
+engine.close()
