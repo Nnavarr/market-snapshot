4cfd26d84447c032fed40847c78514962f73d232
Noe Navarro <eon@Noes-MacBook-Pro.local>
2022-11-27 18:55:08 -0700

    updated connection to include sqlalchemy engine for pandas upload

--- sql_connection.py
+++ sql_connection.py
@@ -1,16 +1,22 @@
 import psycopg2
 import os
+import sqlalchemy
 
 # read password
 with open('./env_var.txt') as f:
   pg_password = f.readlines()
 
 # establish connection to postgresql
-def conn(database):
-  conn = psycopg2.connect(
-    host='localhost',
-    database=database,
-    user='postgres',
-    password=pg_password[0]
-  )
-  return conn
+def conn(database, alchemy=False):
+  if alchemy is False:
+    conn = psycopg2.connect(
+      host='localhost',
+      database=database,
+      user='postgres',
+      password=pg_password[0]
+    )
+    return conn
+  
+  # create sqlalchemy connection (pandas upload)
+  engine = sqlalchemy.create_engine(f'postgresql://postgres:{pg_password[0]}@localhost/{database}')
+  return engine.connect()
--- treasury_rate.py
+++ treasury_rate.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlalchemy
 
 # establish excel filepath
 filepath = r'./excel/MRKTRATE - 09.21.2022.xlsx'
@@ -6,8 +7,20 @@ filepath = r'./excel/MRKTRATE - 09.21.2022.xlsx'
 # import data
 df = pd.read_excel(filepath, sheet_name='Treasury Rates', header=2)
 
-df.head()
-df.info()
-
 # rename columns
 col_names = [x.lower().replace(' ', '_') for x in df.columns.values]
+df.columns = col_names
+
+# drop na values, convert to decimal
+df.dropna(inplace=True)
+for col in col_names[1:]:
+    df[col] = df[col].astype('float') / 100
+
+# upload to sql
+conn = sql_connection.conn('market_data')
+cursor = conn.cursor()
+cursor.close()
+conn.close()
+
+# upload to sql
+df.to_sql('treasury_rates', con=conn, if_exists='replace', index=False)
