49dab3a8990976d55c0cd22d4c97851d486f07b8
Noe Navarro <eon@Noes-MacBook-Pro.local>
2022-11-29 21:52:10 -0700

    separated data processing to be called from a separate python script

---
+++ data_process.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import sql_connection
+from sqlalchemy.types import Integer, Date, DECIMAL
+
+# import file locations
+filepath = r'./excel/MRKTRATE - 09.21.2022.xlsx'
+
+def data_processing(sheet_name: str, header: int, db_name: str, table_name: str):
+  """
+  Author: Noe Navarro
+  Date: 2022-11-29
+  Objective:
+    This function was created in order to process the individual rate files.
+  """
+  df = pd.read_excel(filepath, sheet_name=sheet_name, header=header)
+
+  # rename columns
+  col_names = [x.lower().replace(' ', '_') for x in df.columns.values]
+  df.columns = col_names
+
+  # drop na values, convert to decimal
+  df.dropna(inplace=True)
+  for col in col_names[1:]:
+    df[col] = round(df[col].astype('float') / 100, 4)
+
+  # import schema from sql table
+  engine = sql_connection.conn(db_name, alchemy=True)
+
+  # import schema info for column names
+  schema_info = engine.execute("""
+    SELECT *
+    FROM information_schema.columns
+    WHERE table_name = '{}'
+  ;""".format(table_name)).fetchall()
+
+  # convert to pandas df
+  schema_df = pd.DataFrame(schema_info)
+  col_names = list(schema_df.column_name)
+
+  # update column names within df and upload
+  df.columns = col_names
+
+  # create data types
+  dtype_dict = {}
+  for col in col_names:
+    if col != 'date':
+      dtype_dict.update({col: DECIMAL})
+    else:
+      dtype_dict.update({col: Date})
+
+  # upload to SQL
+  df.to_sql(db_name, con=engine, if_exists='replace', index=False)
+
+  # close connection
+  engine.close()
+
+
