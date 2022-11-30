f09701a85b2744260260b20652208b6a7077ac5e
Noe Navarro <eon@Noes-MacBook-Pro.local>
2022-11-29 22:46:46 -0700

    added main run to treasury_rate.py

--- treasury_rate.py
+++ treasury_rate.py
@@ -6,8 +6,13 @@ import data_process
 # establish excel filepath
 filepath = r'./excel/MRKTRATE - 09.21.2022.xlsx'
 
-try:
-  # process data
-  data_process.data_processing(filepath, sheet_name='Treasury Rates', header=2, db_name='market_data', table_name='treasury_rates')
-except:
-  print('there was an error in the process')
+def main():
+  try:
+    # process data
+    data_process.data_processing(filepath, sheet_name='Treasury Rates', header=2, db_name='market_data', table_name='treasury_rates')
+  except:
+    #TODO: add logging
+    print('there was an error in the process')
+
+if __name__ == '__main__':
+  main()
