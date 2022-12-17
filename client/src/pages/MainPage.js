6172bddb92c1a4f9be2c77d9777173b41a0d4cb3
Noe Navarro <eon@Noes-MacBook-Pro.local>
2022-12-17 14:14:49 -0700

    started development of react front-end

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Treasury from '../components/Treasury';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h2>Testing</h2>
+      <Treasury />
+    </div>
+  )
+}
+
+export default MainPage;
